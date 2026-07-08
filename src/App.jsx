f21db573899adf1543485888bb9b10e7fb1fffa5
heshan <heshansandakelum2018@gmail.com>
2026-07-09 04:36:54 +0530

    initailise page routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
 
 import './App.css'
+import { Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (<div className="min-h-screen bg-slate-900 flex items-center justify-center">
-      <div className="bg-blue-600 text-white text-4xl font-bold px-10 py-6 rounded-xl shadow-xl">
-        TaskSphere
-      </div>
-    </div>
+
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+    </Routes>
   );
 }
 
---
+++ src/pages/Login.jsx
@@ -0,0 +1,8 @@
+function Login(){
+    return(
+        <h1 className="text-3xl font-bold text-center mt-10">
+            Login page
+        </h1>
+    )
+}
+export default Login;
