7c530712a0847ad3bbdfd577471a81216834c3ea
heshan <heshansandakelum2018@gmail.com>
2026-07-09 15:17:09 +0530

    Adding Assitant to page section

---
+++ src/pages/AiAssitant.jsx
@@ -0,0 +1,14 @@
+import AiChat from "../component/ai/AiChat";
+
+
+function AiAssistant(){
+    
+    return (
+        <div style={{ display:"flex", justifyContent:"center", marginTop:"40px" }} >
+            <AiChat/>
+        </div>
+    );
+}
+
+
+export default AiAssistant;
