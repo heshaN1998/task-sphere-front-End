3901bfd34af032f7224ae986f72a231c702ec2db
heshan <heshansandakelum2018@gmail.com>
2026-07-09 14:35:03 +0530

    fixing Bugs

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import ProtectedRoute from './components/layouts/ProtectedRoute';
 import Dashbord from './pages/Dashbord';
+import Users from './pages/Users';
 import Project from './pages/Project';
 import Profile from './pages/Profile';
 import ManagerRoute from './components/layouts/ManagerRoute';
@@ -18,17 +19,23 @@ function App() {
 
   return (
     <Routes>
+      {/* {public routes} */}
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route  element={<ProtectedRoute><AppLayout /></ProtectedRoute>} />
-      <Route path="/dashbord" element={<Dashbord  />} />
-      <Route path="/users" element={<ManagerRoute>Users</ManagerRoute>} />
-      <Route path="/projects" element={<Project />} />
-      <Route path="/profile" element={<Profile />} />
+
+
+      {/* {autheticated routes} */}
+      <Route element={<ProtectedRoute><AppLayout /></ProtectedRoute>}>
+      <Route index element={<Navigate to="/reports" replace />} />
+      <Route path="/dashboard" element={<ManagerRoute><Dashbord /></ManagerRoute>}/>
+      <Route path="/projects" element={<Project />}/>
+      <Route path="/reports" element={<Reports />}/>
+      <Route path="/reports/new" element={<ReportForm />}/>
+      <Route path="/reports/:id/edit" element={<ReportForm />}/>
+      <Route path="/users" element={<ManagerRoute><Users /></ManagerRoute>}/>
+      <Route path="/profile" element={<Profile />}/>
+      </Route>
       <Route path="*" element={<NotFound />} />
-      <Route path ="/reports" element={<Reports />} />
-      <Route path="/reports/new" element={<ReportForm />} />
-      <Route path="/reports/:id/edit" element={<ReportForm />} />
      
     </Routes>
   );
